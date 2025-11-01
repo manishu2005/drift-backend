62cb74026300ba4562db2a6ebd1811145303831c
manishu2005 <142162468+manishu2005@users.noreply.github.com>
2025-11-01 21:32:18 +0530

    Refactor Google authentication strategy
    Refactor Google authentication strategy to handle production and development callback URLs, and improve error handling.

--- utils/googleAuth.js
+++ utils/googleAuth.js
@@ -1,43 +1,47 @@
 import passport from "passport";
-import {Strategy as GoogleStrategy} from 'passport-google-oauth20'
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import userModel from "../models/userModels.js";
+import crypto from "crypto"; 
 
 passport.use(
-    
-    new GoogleStrategy(
-        {
-            
-            clientID: process.env.GOOGLE_CLIENT_ID, 
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
-        callbackURL: '/api/auth/google/callback',
-        },
-
-        async(accessToken, refreshToken, Profiler, done)=>{
-            let user= await userModel.findOne({googleId: Profiler.id});
-            if(!user){
-                user = await userModel.create({
-                    googleId: Profiler.id,
-                    name: Profiler.displayName,
-                    email: Profiler.emails[0].value, 
-                    fullname: {
-                        firstname: Profiler.name.givenName || 'GoogleUser',
-                        lastname: Profiler.name.familyName || 'Default'
-                    },
-                    password: await userModel.hashPassword(crypto.randomUUID()), 
-                });
-            }
-            return done(null, user);
-            
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL:
+        process.env.NODE_ENV === "production"
+          ? "https://drift-backend-3.onrender.com/api/auth/google/callback"
+          : "http://localhost:5000/api/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await userModel.findOne({ googleId: profile.id });
+        if (!user) {
+          user = await userModel.create({
+            googleId: profile.id,
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            fullname: {
+              firstname: profile.name.givenName || "GoogleUser",
+              lastname: profile.name.familyName || "Default",
+            },
+            password: await userModel.hashPassword(crypto.randomUUID()),
+          });
         }
-    )
+        return done(null, user);
+      } catch (err) {
+        done(err, null);
+      }
+    }
+  )
 );
 
-passport.serializeUser((user, done)=>{
-    done(null, user.id)
+passport.serializeUser((user, done) => {
+  done(null, user.id);
 });
 
-passport.deserializeUser((id, done)=>{
-    userModel.findById(id).then((u)=>{
-        done(null,u)
-    });
+passport.deserializeUser((id, done) => {
+  userModel.findById(id).then((u) => {
+    done(null, u);
+  });
 });
